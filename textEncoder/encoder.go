1b6f776847c050b756d070e85336551ee24daf47
rajeshk <rajesh.biswas@esoptra.com>
2021-06-22 12:28:05 +0530

    Updated fetch and TextEncoder.encodeInto.

--- textEncoder/encoder.go
+++ textEncoder/encoder.go
@@ -41,11 +41,13 @@ func NewEncode(opt ...Option) *Encoder {
 	return c
 }
 
+//implements pollyfill -> https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder
 func (c *Encoder) TextEncoderFunctionCallback() v8go.FunctionCallback {
 	return func(info *v8go.FunctionCallbackInfo) *v8go.Value {
 		ctx := info.Context()
 		iso, _ := ctx.Isolate()
 
+		//https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder/encode
 		encodeFnTmp, err := v8go.NewFunctionTemplate(iso, func(info *v8go.FunctionCallbackInfo) *v8go.Value {
 			args := info.Args()
 			if len(args) <= 0 {
@@ -62,31 +64,37 @@ func (c *Encoder) TextEncoderFunctionCallback() v8go.FunctionCallback {
 			return iso.ThrowException(fmt.Sprintf("error creating encode() template: %#v", err))
 		}
 
-		/*
-			encodeIntoFnTmp, err := v8go.NewFunctionTemplate(iso, func(info *v8go.FunctionCallbackInfo) *v8go.Value {
-				args := info.Args()
-				if len(args) <= 0 {
-					return iso.ThrowException(fmt.Sprintf("Expected an arguments\n"))
-				}
-				s := args[0].String()
-				result := args[1].Uint8Array()
-				i := 0
-				for ; i < len(s); i++ {
-					fmt.Printf("%d ", s[i])
-					result[i] = s[i]
-				}
-				v, err := v8go.JSONParse(info.Context(), fmt.Sprintf(`{"read": %d, "written": %d }`, i, i))
-				if err != nil {
-					return iso.ThrowException(fmt.Sprintf("Error parsing json val\n"))
-				}
-
-				return v
-			})
-
-			if err != nil {
-				return iso.ThrowException(fmt.Sprintf("error creating encodeInto() template: %#v", err))
+		//https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder/encodeInto
+		encodeIntoFnTmp, err := v8go.NewFunctionTemplate(iso, func(info *v8go.FunctionCallbackInfo) *v8go.Value {
+			args := info.Args()
+			if len(args) <= 0 {
+				return iso.ThrowException(fmt.Sprintf("Expected an arguments\n"))
 			}
-		*/
+			s := args[0].String()
+			if !args[1].IsArrayBuffer() {
+				return iso.ThrowException(fmt.Sprintf("Expected second argument format as ArrayBuffer\n"))
+			}
+
+			outArray := args[1].ArrayBuffer()
+			result := make([]uint8, len(s)*3)
+			i := 0
+			for ; i < len(s); i++ {
+				fmt.Printf("%d ", s[i])
+				result[i] = s[i]
+			}
+			outArray.PutBytes(result[:i])
+			//return meta info
+			obj := v8go.NewObject(info.Context()) // create object
+			obj.Set("read", int32(i))             // set some properties
+			obj.Set("written", int32(len(result[:i])))
+			fmt.Println("obj set")
+			return obj.Value
+		})
+
+		if err != nil {
+			return iso.ThrowException(fmt.Sprintf("error creating encodeInto() template: %#v", err))
+		}
+
 		resTmp, err := v8go.NewObjectTemplate(iso)
 		if err != nil {
 			return iso.ThrowException(fmt.Sprintf("error creating object template: %#v", err))
@@ -95,11 +103,11 @@ func (c *Encoder) TextEncoderFunctionCallback() v8go.FunctionCallback {
 		if err := resTmp.Set("encode", encodeFnTmp, v8go.ReadOnly); err != nil {
 			return iso.ThrowException(fmt.Sprintf("error setting encode function template: %#v", err))
 		}
-		/*
-			if err := resTmp.Set("encodeInto", encodeIntoFnTmp, v8go.ReadOnly); err != nil {
-				return iso.ThrowException(fmt.Sprintf("error setting encodeInto function template: %#v", err))
-			}
-		*/
+
+		if err := resTmp.Set("encodeInto", encodeIntoFnTmp, v8go.ReadOnly); err != nil {
+			return iso.ThrowException(fmt.Sprintf("error setting encodeInto function template: %#v", err))
+		}
+
 		resObj, err := resTmp.NewInstance(ctx)
 		if err != nil {
 			return iso.ThrowException(fmt.Sprintf("error new instance from ctx: %#v", err))
